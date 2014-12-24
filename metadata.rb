eb4f20ee0774bf98dd5531ef01b04416c7215d95
willejs <williamejsalt@gmail.com>
2014-12-24 00:05:19 +0000

    barebones cookbook

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'amazon-ecs-agent'
+maintainer       'Will Salt'
+maintainer_email 'will@crowdsurge.com'
+license          'Apache 2.0'
+description      'Installs/Configures amazon-ecs-agent'
+long_description 'Installs/Configures amazon-ecs-agent'
+version          '0.1.0'
+
