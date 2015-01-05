9306f5b34b933a43b23318cfcb25abac5b2ce4fc
willejs <williamejsalt@gmail.com>
2015-01-05 16:30:50 +0000

    ammend email address to correct one

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             'amazon-ecs-agent'
 maintainer       'Will Salt'
-maintainer_email 'will@crowdsurge.com'
+maintainer_email 'williamejsalt@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures amazon-ecs-agent'
 long_description 'Installs/Configures amazon-ecs-agent'
 version          '0.1.0'
 
+depends 'docker', '~> 0.36.0'
