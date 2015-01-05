d48dd1ded9e6e0c99442d4c9667424a82e408595
willejs <williamejsalt@gmail.com>
2015-01-05 16:30:34 +0000

    add new node attributes for logging, keys etc

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+default['ecs']['log_folder'] = '/var/log/ecs'
+default['ecs']['log_level'] = 'info'
+default['ecs']['cluster'] = 'default'
+default['ecs']['aws_access_key_id'] = nil
+default['ecs']['aws_secret_access_key'] = nil
