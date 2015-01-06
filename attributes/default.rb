38b004852c4f5e3f518a40dd41467f8b12702f6d
willejs <williamejsalt@gmail.com>
2015-01-06 10:03:45 +0000

    rename attributes to match cookbook name

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default['ecs']['log_folder'] = '/var/log/ecs'
-default['ecs']['log_level'] = 'info'
-default['ecs']['cluster'] = 'default'
-default['ecs']['aws_access_key_id'] = nil
-default['ecs']['aws_secret_access_key'] = nil
+default['amazon-ecs-agent']['log_folder'] = '/var/log/ecs'
+default['amazon-ecs-agent']['log_level'] = 'info'
+default['amazon-ecs-agent']['cluster'] = 'default'
+default['amazon-ecs-agent']['aws_access_key_id'] = nil
+default['amazon-ecs-agent']['aws_secret_access_key'] = nil
