93a73042e8ff55db68b59a23dec5e4acfebf7819
willejs <williamejsalt@gmail.com>
2015-01-06 10:04:36 +0000

    rename attributes and add additional volumes and port mappings

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,7 +19,7 @@
 
 include_recipe 'docker'
 
-directory node['ecs']['log_folder'] do
+directory node['amazon-ecs-agent']['log_folder'] do
 	owner 'docker'
 	group 'docker'
 	mode 0755
@@ -29,10 +29,12 @@ end
 
 docker_container 'ecs-agent' do
   image 'amazon/amazon-ecs-agent'
-  env ["ECS_LOGFILE=#{node['ecs']['log_folder']}/ecs.log",
-  	   "ECS_LOGLEVEL=#{node['ecs']['log_level']}",
-  	   "ECS_CLUSTER=#{node['ecs']['cluster']}",
-  	   "AWS_ACCESS_KEY_ID=#{node['ecs']['aws_access_key_id']}",
-  	   "AWS_SECRET_ACCESS_KEY=#{node['ecs']['aws_secret_access_key']}"
+  env ["ECS_LOGFILE=/log/ecs-agent.log",
+  	   "ECS_LOGLEVEL=#{node['amazon-ecs-agent']['log_level']}",
+  	   "ECS_CLUSTER=#{node['amazon-ecs-agent']['cluster']}",
+  	   "AWS_ACCESS_KEY_ID=#{node['amazon-ecs-agent']['aws_access_key_id']}",
+  	   "AWS_SECRET_ACCESS_KEY=#{node['amazon-ecs-agent']['aws_secret_access_key']}"
     ]
+  volume ["#{node['amazon-ecs-agent']['log_folder']}:/log", '/var/run/docker.sock:/var/run/docker.sock']
+  port '51678:51678'
 end
