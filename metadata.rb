d0205b6636c0b59886ae74b58ef3e4c3b515f582
Kostas Georgiou <kostas.georgiou@sainsburys.co.uk>
2016-06-03 09:36:43 +0100

    Add amazon-ecs-agent.reserved_memory attribute

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'williamejsalt@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures amazon-ecs-agent'
 long_description 'Installs/Configures amazon-ecs-agent'
-version '1.2.1'
+version '1.2.2'
 
 supports 'ubuntu'
 
